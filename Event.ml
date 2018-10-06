dea3ad5b9a009fb77c19c727029b368335a7c12f
P-Russell <pdrussell011@gmail.com>
2018-10-06 13:41:00 +0200

    working basic 3 x 3 game with out input validation

--- Event.ml
+++ Event.ml
@@ -6,20 +6,12 @@
 (*   By: prussell <marvin@42.fr>                    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2018/10/06 07:30:44 by prussell          #+#    #+#             *)
-(*   Updated: 2018/10/06 13:14:09 by prussell         ###   ########.fr       *)
+(*   Updated: 2018/10/06 13:36:50 by prussell         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
-(*
- * let rec loop map =
-     * Print.map_and_instructions
-     * let input = Input.get
-     * if (is_winning_move map input)
-     * loop (update_map(map input)) player
-*)
-
 let update_map map usr_in player = match usr_in with
-    | h::n::t -> List.mapi(fun i x -> if i = (h * 3 + n) then player else x) map
+    | h::n::t -> List.mapi(fun i x -> if i = (n * 3 + h) then player else x) map
     | _ -> []
 
 let won_state player =
@@ -31,7 +23,6 @@ let won_state player =
         [ '/'; '-' ; '\\';
           '|'; ' ' ; '|';
           '\\'; '_' ; '/'; 'O']
-
 
 let user_x () = 
     print_string "x: ";
@@ -45,38 +36,46 @@ let is_winning_state map =
    match map with 
    | [ a; _; _; 
        b; _; _; 
-       c; _; _; _ ] when a = b && b = c -> true
+       c; _; _; _ ] when a = b && b = c && a <> '-' -> true
    | [ _; a; _; 
        _; b; _; 
-       _; c; _; _ ] when a = b && b = c -> true
+       _; c; _; _ ] when a = b && b = c && a <> '-' -> true
    | [ _; _; a; 
        _; _; b; 
-       _; _; c; _ ] when a = b && b = c -> true
+       _; _; c; _ ] when a = b && b = c && a <> '-' -> true
    | [ a; b; c; 
        _; _; _; 
-       _; _; _; _ ] when a = b && b = c -> true
+       _; _; _; _ ] when a = b && b = c && a <> '-' -> true
    | [ _; _; _; 
        a; b; c; 
-       _; _; _; _ ] when a = b && b = c -> true
+       _; _; _; _ ] when a = b && b = c && a <> '-' -> true
    | [ _; _; _; 
        _; _; _; 
-       a; b; c; _ ] when a = b && b = c -> true
+       a; b; c; _ ] when a = b && b = c && a <> '-' -> true
    | [ a; _; _; 
        _; b; _; 
-       _; _; c; _ ] when a = b && b = c -> true
+       _; _; c; _ ] when a = b && b = c && a <> '-' -> true
    | [ _; _; a; 
        _; b; _; 
-       c; _; _; _ ] when a = b && b = c -> true
+       c; _; _; _ ] when a =  b && b = c && a <> '-' -> true
    | [ 'O' | 'X' ; 'O' | 'X'; 'O' | 'X'; 
        'O' | 'X' ; 'O' | 'X'; 'O' | 'X'; 
        'O' | 'X' ; 'O' | 'X'; 'O' | 'X'; _ ] -> true;
    | _ -> false
 
 let rec loop map player usr_x usr_y =
-    Print.map_instructions map player; 
+    if (is_winning_state map) then
+        if player = 'X' then
+            Print.map (won_state 'O')
+        else
+            Print.map (won_state 'X')
 
-    let u_x = user_x () and u_y = user_y () in
-    if player = 'X' then
-        loop (update_map map [u_x; u_y] player) 'O' (u_x)(u_y)
     else
-        loop (update_map map [u_x; u_y] player) 'X' (u_x)(u_y)
+        begin
+        Print.map_instructions map player; 
+        let u_x = user_x () and u_y = user_y () in
+        if player = 'X' then
+            loop (update_map map [u_x; u_y] player) 'O' (u_x)(u_y)
+        else
+            loop (update_map map [u_x; u_y] player) 'X' (u_x)(u_y)
+        end
