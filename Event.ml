5f82291f7b9b2cfb3a571ef85e4f05a18913e4d5
P-Russell <pdrussell011@gmail.com>
2018-10-06 12:13:24 +0200

    not working

--- Event.ml
+++ Event.ml
@@ -6,7 +6,7 @@
 (*   By: prussell <marvin@42.fr>                    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2018/10/06 07:30:44 by prussell          #+#    #+#             *)
-(*   Updated: 2018/10/06 10:13:17 by prussell         ###   ########.fr       *)
+(*   Updated: 2018/10/06 12:12:56 by prussell         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -18,8 +18,9 @@
      * loop (update_map(map input)) player
 *)
 
-let update_map map inputX inputY player = 
-    List.mapi(fun i x -> if i = (inputX + inputY + 2) then player else x) map
+let update_map map usr_in player = match usr_in with
+    | h::n::t -> List.mapi(fun i x -> if i = (h + n + 1) then player else x) map
+    | _ -> []
 
 let won_state player =
     if player = 'X' then
@@ -30,7 +31,6 @@ let won_state player =
         [ '/'; '-' ; '\\';
           '|'; ' ' ; '|';
           '\\'; '_' ; '/'; 'O']
-
 
 let is_winning_state map =
    match map with 
@@ -63,17 +63,15 @@ let is_winning_state map =
        'O' | 'X' ; 'O' | 'X'; 'O' | 'X'; _ ] -> true;
    | _ -> false
 
-
-let rec loop map player= 
-    Print.mapAndInstructions map player;
-
-    Print.mapAndInstructions (update_map map 1 2 'X') 'X';
-    Print.mapAndInstructions (update_map map 1 1 'X') 'X';
-    Print.mapAndInstructions (update_map map 3 3 'X') 'X'
+let rec loop map player usr_in =
+    Print.map map
 (*
-    if player = 'X' then
-        loop map 'O'
-    else
-        loop map 'X'
+    let () = print_char player in
+    let () = print_string "'s turn to play" in
+    let () = print_newline in  
 
-        *)
+    if player = 'X' then
+        loop map 'O' user_input
+    else
+        loop map 'X' user_input
+*)
--- Print.ml
+++ Print.ml
@@ -6,7 +6,7 @@
 (*   By: prussell <marvin@42.fr>                    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2018/10/06 07:19:41 by prussell          #+#    #+#             *)
-(*   Updated: 2018/10/06 09:14:04 by prussell         ###   ########.fr       *)
+(*   Updated: 2018/10/06 12:11:29 by prussell         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -21,11 +21,13 @@ let rec map = function
                        map tl;
     | _ -> ()
 
-
 let instructions player = 
     print_char player;
     print_endline "'s turn to play"
 
+let usr_input u_in = match u_in with
+    | h::n::t -> print_int h; print_char ' '; print_int n; print_char '\n'
+    | _ -> print_endline "Nothing"
 
 let mapAndInstructions m toPlay = 
     map m;
