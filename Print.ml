f86b470d92efbcbb1e302d4d91f46e28ef26e585
P-Russell <pdrussell011@gmail.com>
2018-10-07 11:17:27 +0200

    print map

--- Print.ml
+++ Print.ml
@@ -11,7 +11,7 @@
 (* ************************************************************************** *)
 
 
-let print_line  = function
+let print_line_first_three  = function
     | [] -> () 
     | v1::v2::v3::tl -> print_char v1;
                         print_char ' ';
@@ -21,26 +21,52 @@ let print_line  = function
                         print_char '|';
     | _ -> ()
 
-let rec print_line1  = function
+let print_line_second_three  = function
     | [] -> () 
-    | v1::v2::v3::tl -> print_line [v1; v2; v3];
-                        print_line1 tl;
+    | v1::v2::v3::v4::v5::v6::tl -> print_char v4;
+                        	    print_char ' ';
+                          	    print_char v5;
+                                    print_char ' ';
+                                    print_char v6;
+                                    print_char '|';
     | _ -> ()
 
 
+let print_line_last_three  = function
+    | [] -> () 
+    | v1::v2::v3::v4::v5::v6::v7::v8::v9::tl -> print_char v7;
+                       				print_char ' ';
+                       				print_char v8;
+                       				print_char ' ';
+                        			print_char v9;
+                                    		print_char '|';
+    | _ -> ()
+(*
 let rec extract_from_three = function
     | [] -> print_endline "-------------------"
-    | n1::n2::n3::tl -> print_line1 n1; 
-                        print_line1 n2;
-                        print_line1 n3;
+    | n1::n2::n3::tl -> 
+    			print_line n1; 
+                        print_line n2;
+                        print_line n3;
                         print_char '\n';
                         extract_from_three tl;
     | _ -> () 
-
+*)
 let rec map = function
     | [] -> ()
-    | h::n1::n2::tl -> extract_from_three [h; n1; n2];
-                       map tl;
+    | h::n1::n2::tl ->   print_line_first_three h;
+    			 print_line_first_three n1;
+    			 print_line_first_three n2;
+                    	 print_char '\n';
+			 print_line_second_three h;
+			 print_line_second_three n1;
+			 print_line_second_three n2;
+                    	 print_char '\n';
+			 print_line_last_three h;
+			 print_line_last_three n1;
+			 print_line_last_three n2;
+                    	 print_char '\n';
+                      	 map tl
     | _ -> ()
 
 let instructions player = 
