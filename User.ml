0608b888f5b41cbb6e78ff8e2c84cb4d4d25fea1
P-Russell <pdrussell011@gmail.com>
2018-10-07 15:33:47 +0200

    input validation

--- User.ml
+++ User.ml
@@ -20,11 +20,7 @@ let check_small_board board num =
 	if (num < 1 || num >= List.length board) then
 		false
 	else if (List.nth board (num - 1)) = '-' then
-		begin
-		print_char (List.nth board (num - 1));
-		print_char '\n';
 		true
-		end
 	else
 		false
 
@@ -38,7 +34,13 @@ let check_valid_board map num =
 
 let rec board map = 
 	print_string "board number: ";
-	let x = int_of_string(read_line()) in
+	let x = 
+		try
+			int_of_string(read_line())
+		with Failure "int_of_string" ->
+			print_endline "Invalid input";
+			board map
+		in
 		if (check_valid_board map x) then
 			x
 		else
@@ -49,7 +51,13 @@ let rec board map =
 
 let rec square map board_n = 
 	print_string "square number: ";
-	let x  = int_of_string(read_line()) in
+	let x  =
+		try
+			int_of_string(read_line())
+		with Failure "int_of_string" ->
+			print_endline "Invalid input";
+			square map board_n
+		in
 		if (check_small_board (List.nth map (board_n - 1)) x ) then
 			x
 		else
